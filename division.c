910adff68721dc8aaba246a777dadee155a31d8d
sahil2905 <76366874+sahil2905@users.noreply.github.com>
2021-03-21 16:25:58 +0530

    Update division.c

--- division.c
+++ division.c
@@ -1,8 +1,11 @@
+//Author:Sahil kumar
+//Date:20 march 2021
+//purpose of the is to find division of two number.
 #include<stdio.h>
-int main(){
-	float number1,number2,div;
-	scanf("%f %f",&number1,&number2);
-	div=number1/number2;
-	printf("\n Division of 2 numbers=%f",div);
-	return 0;
-}
+int main(){// main function body begin
+	float number1,number2,div;//variable decleration.
+	scanf("%f %f",&number1,&number2);//scanning the input from user
+	div=number1/number2;// In this line number1 is div with number2
+	printf("\n Division of 2 numbers=%f",div);//printing the output after taking the input from user
+	return 0;//return statement
+}//main function body ends
