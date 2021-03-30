914617dc7c5a2dccf00bf073e7310746ee9ca442
sahil2905 <76366874+sahil2905@users.noreply.github.com>
2021-03-30 19:49:40 +0530

    Update 8_factorial.c

--- 8_factorial.c
+++ 8_factorial.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-int main(){
-	int i,fact=1,number;
-	printf("Enter a number");
-	scanf("%d",&number);
-	 for(i=1;i<=number;i++){
-	 fact=fact*i;
-	 }
-printf("Factorial of %d is:%d",number,fact);
-return 0;	 
-}
+#include<stdio.h>//preprocessor directive to include standard input and output header file
+int main(){//main function body begin
+	int i,fact=1,number;//variable deceleration
+	printf("Enter a number");//printing the output after taking input fron user
+	scanf("%d",&number);//scanning input from the user
+	 for(i=1;i<=number;i++){//using for loop 
+	 fact=fact*i;//initilation
+	 }//end of loop body
+printf("Factorial of %d is:%d",number,fact);//printing output of factorial 
+return 0;//return statement
+}//end of main function body
