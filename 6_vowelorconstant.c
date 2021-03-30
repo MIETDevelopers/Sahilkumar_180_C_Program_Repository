f28a9eacbaa8a67bb8f7110f8ea6b416d456e137
sahil2905 <2020a1r180@mietjammu.in>
2021-03-30 17:16:58 +0530

    add helloworld to repository

--- 6_vowelorconstant.c
+++ 6_vowelorconstant.c
@@ -1,24 +1,21 @@
-//Author:Sahil kumar.
-//Date:20 march 2021
-//Purpose of the program is to find identify upper and lower case Vowel.
-#include <stdio.h>//preprocessor directive to include standard input and output header file.
+#include <stdio.h>
 
-int main()//main function body beigns
+int main()
 {
     char ch;
-    int lowercase_Vowel, uppercase_Vowel;//variables declaration
+    int lowercase_Vowel, uppercase_Vowel;
 
-    printf("Please Enter an alphabet: \n");//printing the output after taking the the input from users
-    scanf(" %c", &ch);//It read the input taken from users
+    printf("Please Enter an alphabet: \n");
+    scanf(" %c", &ch);
 
-    lowercase_Vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');// giving lowercase value to the program for output. 
-    uppercase_Vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');// giving uppercase value to the program for output.
+    lowercase_Vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    uppercase_Vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
 
-    if (lowercase_Vowel || uppercase_Vowel) {//In this we are not sure that the no enter by usee is vowel or constant.
-    	printf("\n %c is a VOWEL.", ch);//printing the output if number entered by user is vowel.
+    if (lowercase_Vowel || uppercase_Vowel) {
+    	printf("\n %c is a VOWEL.", ch);
 	}
     else {
-    	printf("\n %c is a CONSONANT.", ch);//printing the output after the else whether the no is vowel or constant depending upon user input.
+    	printf("\n %c is a CONSONANT.", ch);
 	}
-    return 0;//return statement
-} //End of main function body
+    return 0; 
+} 
