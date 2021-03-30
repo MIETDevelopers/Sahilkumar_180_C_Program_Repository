b68c43934836f95901e42c7225f7d875a006954a
sahil2905 <2020a1r180@mietjammu.in>
2021-03-30 20:13:29 +0530

    add euclidean to repository

---
+++ 3_do_while .c
@@ -0,0 +1,17 @@
+//Author:Sahil kumar
+//Date:29 march 2021
+//purpose of the program is to add number until the user enter zero by using do while loop.
+#include<stdio.h>  //preprocess directive to include standard input and output header file
+int main(){  //main function body begin
+	double number ,sum=0; //variable decleration
+	// the body of the loop is executed at least once
+	do
+	{
+		printf("Enter a number: ");//printing the output after takking the input from user
+		scanf("%if", &number);//scan the input from user 
+		sum += number;
+	}
+	while(number !=0.0);//while loop executing
+	prinf("Sum=%.2if",sum);//print the output after taking input from user
+	return 0; //return statement
+} 	// end of main function body
