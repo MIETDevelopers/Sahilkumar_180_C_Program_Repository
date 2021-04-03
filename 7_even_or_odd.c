1ead81c325e41616bdae1a1fd78f0a8aecabd6eb
sahil2905 <76366874+sahil2905@users.noreply.github.com>
2021-04-03 16:17:32 +0530

    Update and rename 5_even_or_odd.c to 7_even_or_odd.c

---
+++ 7_even_or_odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//include standard input and output header file
+int main(){//main function body
+	int num;//deceleartion
+	printf("Enter an integer : ");// print output of integer depend on user input 
+	scanf("%d", &num);// scanning the input 
+    if ( num%2 == 0)//if statement use 
+     printf("%d is an even.", num);// printing output if number input by user is even
+    else//else statement use
+     printf("%d is an odd.",num);//printing output if number input by user is odd 
+    return 0; //return statement 
+}// end of main function body
