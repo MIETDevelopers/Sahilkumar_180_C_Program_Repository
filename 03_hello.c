aaa80019e84ed0b8cd9fc64267e12c97e6693b4a
sahil2905 <2020a1r180@mietjammu.in>
2021-04-04 15:21:41 +0530

    add file to repository

---
+++ 03_hello.c
@@ -0,0 +1,8 @@
+//Author: Sahil Kumar 
+//creation date: 16th march 2021
+// purpose of program is to print Helloworld.
+#include<stdio.h> // preprocessive directive to include standard input and output header file
+int main(){ // main function boby begin
+	printf("Helloworld");//printf statement used to print the output of the program
+	return 0; // return statement
+}// end of main function body
