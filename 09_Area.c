88f1dd9c81783632a18f87be1bbe17d868a4a0e8
sahil2905 <2020a1r180@mietjammu.in>
2021-04-04 17:32:24 +0530

    add file to repository

---
+++ 09_Area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.14
+int main(){
+    float radius,area;
+    printf("Enter radius of circle\n");
+    scanf("%f",&radius);
+    area=PI*radius*radius;
+    printf("Area of circle is %f\n",area);
+    return 0;
+
+}
